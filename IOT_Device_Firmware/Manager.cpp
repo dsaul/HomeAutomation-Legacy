ea9122afd25bbe752426399ca5afa7279573d9b0
Dan Saul <dan@dsaul.ca>
2017-12-28 16:00:39 -0600

    compartimentalize more stuff

--- IOT_Device_Firmware/Manager.cpp
+++ IOT_Device_Firmware/Manager.cpp
@@ -8,32 +8,14 @@ Pin pins[] = {
 	Pin("0", D0, kPinUseCaseOutputPrimary)
 };
 
-// Default Server Values
-// These are overriten by config.json
-char cncServer[40];
-char cncPort[6] = "";
-char cncSecret[34] = "";
-
-WiFiUDP _udpSocket;
-char _packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet
-
-
-//flag for saving data
-bool shouldSaveConfig = false;
-
-//callback notifying us of the need to save config
-void saveConfigCallback () {
-	Serial.println("Should save config");
-	shouldSaveConfig = true;
-}
-
-
-
-
-
 Manager::Manager()
 {
+	// Default Server Values
+	// These are overriten by config.json
 	
+	strlcpy(cncServer, "", sizeof(cncServer));
+	strlcpy(cncPort, "", sizeof(cncPort));
+	strlcpy(cncSecret, "", sizeof(cncSecret));
 }
 
 Manager::~Manager()
@@ -43,6 +25,8 @@ Manager::~Manager()
 
 void Manager::DoSetup()
 {
+	Serial.printf("** %d\n",sizeof(cncServer));
+	
 	// Let the individual pins set themselves up.
 	for (int i = 0; i < sizeof(pins) / sizeof(*pins); i++) {
 		pins[i].DoSetup();
@@ -51,140 +35,12 @@ void Manager::DoSetup()
 	
 	pinMode(2, OUTPUT);
 
+	DoSetupSPIFFS();
+	DoSetupWiFiManager();
 	
-	
-	//clean FS, for testing
-	//SPIFFS.format();
-
-	//read configuration from FS json
-	Serial.println("mounting FS...");
-
-	if (SPIFFS.begin()) {
-		Serial.println("mounted file system");
-		if (SPIFFS.exists("/config.json")) {
-			//file exists, reading and loading
-			Serial.println("reading config file");
-			File configFile = SPIFFS.open("/config.json", "r");
-			if (configFile) {
-				Serial.println("opened config file");
-				size_t size = configFile.size();
-				// Allocate a buffer to store contents of the file.
-				std::unique_ptr<char[]> buf(new char[size]);
-
-				configFile.readBytes(buf.get(), size);
-				DynamicJsonBuffer jsonBuffer;
-				JsonObject& json = jsonBuffer.parseObject(buf.get());
-				json.printTo(Serial);
-				if (json.success()) {
-					Serial.println("\nparsed json");
-
-					strcpy(cncServer, json["cncServer"]);
-					strcpy(cncPort, json["cncPort"]);
-					strcpy(cncSecret, json["cncSecret"]);
-
-				} else {
-					Serial.println("failed to load json config");
-				}
-			}
-		}
-	} else {
-		Serial.println("failed to mount FS");
-	}
-	//end read
-
-
-
-	// The extra parameters to be configured (can be either global or just in the setup)
-	// After connecting, parameter.getValue() will get you the configured value
-	// id/name placeholder/prompt default length
-	WiFiManagerParameter custom_cnc_server("CncIP", "CNC Server", cncServer, 40);
-	WiFiManagerParameter custom_cnc_port("CncPort", "CNC Port", cncPort, 6);
-	WiFiManagerParameter custom_cnc_token("CncToken", "CNC Token", cncSecret, 32);
-
-	//WiFiManager
-	//Local intialization. Once its business is done, there is no need to keep it around
-	WiFiManager wifiManager;
-	
-	//set config save notify callback
-	wifiManager.setSaveConfigCallback(saveConfigCallback);
-
-	//set static ip
-	//wifiManager.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
-
-	Serial.print("WiFi MAC = ");
-	Serial.println(WiFi.macAddress());
-	
-	//add all your parameters here
-	wifiManager.addParameter(&custom_cnc_server);
-	wifiManager.addParameter(&custom_cnc_port);
-	wifiManager.addParameter(&custom_cnc_token);
-
-	//reset settings - for testing
-	//wifiManager.resetSettings();
-
-	//set minimu quality of signal so it ignores AP's under that quality
-	//defaults to 8%
-	//wifiManager.setMinimumSignalQuality();
-	
-	//sets timeout until configuration portal gets turned off
-	//useful to make it all retry or go to sleep
-	//in seconds
-	//wifiManager.setTimeout(120);
-
-	//fetches ssid and pass and tries to connect
-	//if it does not connect it starts an access point with the specified name
-	//here	"AutoConnectAP"
-	//and goes into a blocking loop awaiting configuration
-	char ssid[32] = "IOT Device ";
-	strcat(ssid, WiFi.macAddress().c_str());
-	if (!wifiManager.autoConnect(ssid, "password")) {
-		Serial.println("failed to connect and hit timeout");
-		delay(3000);
-		//reset and try again, or maybe put it to deep sleep
-		ESP.reset();
-		delay(5000);
-	}
-
-	//if you get here you have connected to the WiFi
-	Serial.println("connected...yeey :)");
-
-	//read updated parameters
-	strcpy(cncServer, custom_cnc_server.getValue());
-	strcpy(cncPort, custom_cnc_port.getValue());
-	strcpy(cncSecret, custom_cnc_token.getValue());
-
-	//save the custom parameters to FS
-	if (shouldSaveConfig) {
-		Serial.println("saving config");
-		DynamicJsonBuffer jsonBuffer;
-		JsonObject& json = jsonBuffer.createObject();
-		json["cncServer"] = cncServer;
-		json["cncPort"] = cncPort;
-		json["cncSecret"] = cncSecret;
-
-		File configFile = SPIFFS.open("/config.json", "w");
-		if (!configFile) {
-			Serial.println("failed to open config file for writing");
-		}
-
-		json.printTo(Serial);
-		json.printTo(configFile);
-		configFile.close();
-		//end save
-	}
-
-	Serial.println("local ip");
-	Serial.println(WiFi.localIP());
-
-
-	
-	
-
-	_udpSocket.begin(1234);
+	udpSocket.begin(1234);
 
 	digitalWrite(2, HIGH); // for some reason pin 2 has to be high instead of low to be off?
-	
-	
 	
 }
 
@@ -199,36 +55,30 @@ void Manager::DoLoop()
 		SendStatus();
 	}
 	
-	
-	
-	
-	
-	
-	
 	// if there's data available, read a packet
-	int packetSize = _udpSocket.parsePacket();
+	int packetSize = udpSocket.parsePacket();
 	//Serial.printf("packetSize: %i\n",packetSize);
 	
 	if (packetSize) {
 		//Serial.print("Received packet of size ");
 		//Serial.println(packetSize);
 		//Serial.print("From ");
-		//IPAddress remoteIp = _udpSocket.remoteIP();
+		//IPAddress remoteIp = udpSocket.remoteIP();
 		//Serial.print(remoteIp);
 		//Serial.print(", port ");
-		//Serial.println(_udpSocket.remotePort());
+		//Serial.println(udpSocket.remotePort());
 
 		// read the packet into packetBufffer
-		int len = _udpSocket.read(_packetBuffer, UDP_TX_PACKET_MAX_SIZE);
+		int len = udpSocket.read(packetBuffer, UDP_TX_PACKET_MAX_SIZE);
 		if (len > 0) {
-			_packetBuffer[len] = 0;
+			packetBuffer[len] = 0;
 		}
 		//Serial.println("Contents:");
-		//Serial.println(_packetBuffer);
+		//Serial.println(packetBuffer);
 		
 		
 		StaticJsonBuffer<UDP_TX_PACKET_MAX_SIZE> jsonBuffer;
-		JsonObject& root = jsonBuffer.parseObject(_packetBuffer);
+		JsonObject& root = jsonBuffer.parseObject(packetBuffer);
 		if (!root.success()) {
 			Serial.println("Parsing of packet failed!");
 		} else {
@@ -255,12 +105,9 @@ void Manager::DoLoop()
 		
 		
 	}
-
-	
-
 	
 	
-	//delay(50);
+	
 }
 
 
@@ -305,10 +152,10 @@ void Manager::SendStatus() {
 	root["MAC"] = WiFi.macAddress();
 	
 	//Serial.printf("heartbeat @ %i\n", millis());
-	_udpSocket.beginPacket(cncServer, atoi(cncPort));
-	root.printTo(_udpSocket);
-	_udpSocket.print('\n');
-	_udpSocket.endPacket();
+	udpSocket.beginPacket(cncServer, atoi(cncPort));
+	root.printTo(udpSocket);
+	udpSocket.print('\n');
+	udpSocket.endPacket();
 }
 
 void Manager::SendACK(const char * uid) {
@@ -321,10 +168,10 @@ void Manager::SendACK(const char * uid) {
 	root["uid"] = uid;
 	
 	//Serial.printf("heartbeat @ %i\n", millis());
-	_udpSocket.beginPacket(cncServer, atoi(cncPort));
-	root.printTo(_udpSocket);
-	_udpSocket.print('\n');
-	_udpSocket.endPacket();
+	udpSocket.beginPacket(cncServer, atoi(cncPort));
+	root.printTo(udpSocket);
+	udpSocket.print('\n');
+	udpSocket.endPacket();
 }
 
 template <class T> void Manager::SendNotify(const char * event, const char * dataKey, T dataValue) {
@@ -338,10 +185,10 @@ template <class T> void Manager::SendNotify(const char * event, const char * dat
 	root[dataKey] = dataValue;
 	
 	//Serial.printf("heartbeat @ %i\n", millis());
-	_udpSocket.beginPacket(cncServer, atoi(cncPort));
-	root.printTo(_udpSocket);
-	_udpSocket.print('\n');
-	_udpSocket.endPacket();
+	udpSocket.beginPacket(cncServer, atoi(cncPort));
+	root.printTo(udpSocket);
+	udpSocket.print('\n');
+	udpSocket.endPacket();
 }
 
 
--- IOT_Device_Firmware/Manager.h
+++ IOT_Device_Firmware/Manager.h
@@ -6,23 +6,42 @@
 
 
 class Manager {
+	/*
+	** Variables
+	*/
+	
 	public:
 		
+	char cncServer[40];
+	char cncPort[6];
+	char cncSecret[34];
+	
+	private:
+	WiFiUDP udpSocket;
+	char packetBuffer[UDP_TX_PACKET_MAX_SIZE]; //buffer to hold incoming packet
+	
+	/*
+	** Member Functions
+	*/
+	public:
+	Manager();
+	~Manager();
 
-		Manager();
-		~Manager();
+	void DoSetup();
+	void DoLoop();
 
-		void DoSetup();
-		void DoLoop();
+	void EnableId(const char *id);
+	void DisableId(const char *id);
 
-		void EnableId(const char *id);
-		void DisableId(const char *id);
-
-		void SendStatus();
-		void SendACK(const char *uid);
-		template <class T> void SendNotify(const char *event, const char *dataKey, T dataValue);
-		void HandleEnableIndexCMD(JsonObject &root);
-		void HandleDisableIndexCMD(JsonObject &root);
+	void SendStatus();
+	void SendACK(const char *uid);
+	template <class T> void SendNotify(const char *event, const char *dataKey, T dataValue);
+	void HandleEnableIndexCMD(JsonObject &root);
+	void HandleDisableIndexCMD(JsonObject &root);
+	
+	private:
+	void DoSetupSPIFFS();
+	void DoSetupWiFiManager();
 };
 
 #endif
